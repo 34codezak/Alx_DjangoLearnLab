8b2f83af3e2f97b72e60268ff792ba8a821d62f9
34codezak <zacharyz4qke@gmail.com>
2025-02-26 09:36:13 +0300

    Push new models

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,6 +4,7 @@ from django.views.generic.detail import DetailView
 from .models import Library
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from django.contrib.auth.decorators import user_passes_test
 
 def register(request):
     if request.method == 'POST':
@@ -27,3 +28,25 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+    
+    
+def check_admin(user):
+    return user.userprofile.role == 'Admin'
+
+def check_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+def check_member(user):
+    return user.userprofile.role == 'Member'
+
+@user_passes_test(check_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(check_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(check_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
