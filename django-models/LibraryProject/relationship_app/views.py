4904dcbf437818f2408054b7068b476307bb1cdc
34codezak <zacharyz4qke@gmail.com>
2025-02-26 09:59:19 +0300

    Push new models

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,7 +4,7 @@ from django.views.generic.detail import DetailView
 from .models import Library
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
-from django.contrib.auth.decorators import user_passes_test, permission_required
+from django.contrib.auth.decorators import permission_required
 from .forms import BookForm
 
 @permission_required('relationship_app.can_add_book', raise_exception=True)
