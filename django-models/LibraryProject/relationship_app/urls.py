143f1d6ccc261ba89d66371726e0402f4d9cf53d
34codezak <zacharyz4qke@gmail.com>
2025-02-26 11:45:54 +0300

    Push new models

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('librarian-view/', librarian_view, name='librarian_view'),
     path('member-view/', member_view, name='member_view'),
     path('add_book/', views.add_book, name='add_book'),
-    path('book/<int:pk>/edit/', views.edit_book, name='edit_book'),
+    path('edit_book/', views.edit_book, name='edit_book'),
     path('book/<int:pk>/delete/', views.delete_book, name='delete_book'),
     path('', views.book_list, name='book_list'), 
 ]
