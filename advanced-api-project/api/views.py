db7f4a7c49b034468e8b0bfb28f3d319b187c169
34codezak <zacharyz4qke@gmail.com>
2025-03-14 08:17:58 +0300

    Updated the project with views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework import viewsets, generics
 
-# Create your views here.
+#ListView: Retrieve a list of all books
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
+#DetailView: Retrieve a single book by its ID
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
+#CreateView: Create a new book
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
+#UpdateView: Update an existing book by its ID
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
+#DeleteView: Delete an existing book by its ID
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
