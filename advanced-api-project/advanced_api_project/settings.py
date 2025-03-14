069192c34a9ba73a462b4e37f98d847034aa9cd1
34codezak <zacharyz4qke@gmail.com>
2025-03-14 08:04:31 +0300

    Api project with serializers added

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework", # Added Django REST framework
+    "api", # Added custom API app
 ]
 
 MIDDLEWARE = [
