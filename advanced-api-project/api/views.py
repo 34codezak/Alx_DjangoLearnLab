d3a1944f987859e7409ae42eb409099fae26896b
34codezak <zacharyz4qke@gmail.com>
2025-03-14 10:32:25 +0300

    Updated api project with Custom Views and Generic Views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -13,7 +13,7 @@ from django_filters import rest_framework
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, OrderingFilter] # These are the filters applied to the view
     filterset_fields = ['title', 'author' 'publication_year']
     search_fields = ['title', 'author__name']
     ordering_fields = ['title', 'publication_year'] # These are the fields allowed for filtering and ordering
