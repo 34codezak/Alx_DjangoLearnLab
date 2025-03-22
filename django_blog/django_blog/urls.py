c1ed3badf29a076a115bb33898f9e4090090c1ba
34codezak <zacharyz4qke@gmail.com>
2025-03-22 22:28:59 +0300

    Add Blog Post Management Features

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -23,6 +23,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('blog.urls')),
+    path("post/<int:pk>/delete/"), 
+    path("post/<int:pk>/update/"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
