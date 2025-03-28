33e1fcd6dea42d9cd590f95b67a6b5d5b1670b8f
34codezak <zacharyz4qke@gmail.com>
2025-03-28 09:34:45 +0300

    Updated the post and comment models to add text fields

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    content = models.TextField(max_length=500)
+    content = models.TextField()
     author = models.ForeignKey("accounts.CustomUser", on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -11,7 +11,7 @@ class Post(models.Model):
         return self.title
     
 class Comment(models.Model):
-    content = models.TextField(max_length=500)
+    content = models.TextField()
     author = models.ForeignKey("accounts.CustomUser", on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
