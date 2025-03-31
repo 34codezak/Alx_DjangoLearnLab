2b66b18efb4255939080ba38841b8e4dd0a9d6f7
34codezak <zacharyz4qke@gmail.com>
2025-03-31 08:44:14 +0300

    Added a notifications and a like model in posts

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from accounts.models import CustomUser
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
 class Post(models.Model):
     author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='posts')
@@ -21,3 +22,14 @@ class Comment(models.Model):
 
     def __str__(self):
         return f'Comment by {self.author.username} on {self.post.title}'
+    
+class Like(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    post = models.ForeignKey("posts.Post", on_delete=models.CASCADE, related_name="likes")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'post')  # Ensures a user can like a post only once
+
+    def __str__(self):
+        return f"{self.user} liked {self.post}"
