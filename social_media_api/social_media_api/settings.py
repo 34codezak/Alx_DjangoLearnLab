8a4d3e9624f886fa7a978929da7cf3a24d699266
34codezak <zacharyz4qke@gmail.com>
2025-03-31 09:14:39 +0300

    Modified the DEBUG setting from True to False

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-sq^x3*nql+!q#p-7r&m6lk0!etkvrrkdv%(rrib9afhms!@v+j"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
