3c4483c732087505416f7508b27cba3a878a1024
34codezak <zacharyz4qke@gmail.com>
2025-03-31 08:55:48 +0300

    Added like and Unlike views

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet, user_feed
-from posts.views import CustomUser
+from posts.views import CustomUser, LikePostView, UnlikePostView
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
@@ -9,5 +9,7 @@ router.register(r'comments', CommentViewSet)
 
 urlpatterns = urlpatterns = [
     path('feed/', user_feed, name='feed'),
-    path('User', CustomUser, name='CustomUser')
+    path('User', CustomUser, name='CustomUser'),
+    path("<int:pk>/like/", LikePostView.as_view(), name="like-post"),
+    path("<int:pk>/unlike/", UnlikePostView.as_view(), name="unlike-post"),
 ] + router.urls
