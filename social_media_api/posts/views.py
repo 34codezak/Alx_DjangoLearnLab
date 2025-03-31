0dc3c29041d07101426a215761990ce854a8caec
34codezak <zacharyz4qke@gmail.com>
2025-03-31 08:52:18 +0300

    Added a notifications and a like model in posts

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -71,7 +71,7 @@ class UnlikePostView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, pk):
-        post = get_object_or_404(Post, pk=pk)
+        post = generics.get_object_or_404(Post, pk=pk)
         like = Like.objects.filter(user=request.user, post=post)
 
         if like.exists():
