834c7e1e6b2f3a6d37bddabfc31f566cb39f06e1
34codezak <zacharyz4qke@gmail.com>
2025-03-31 07:40:57 +0300

    Corrected a circular CustomUser import

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,27 +1,31 @@
 from rest_framework import serializers
 from .models import CustomUser
+from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ["username", "email", "bio", "profile_picture", "followers"]
-        
+        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
+
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ["username", "email", "password"]
-        extra_kwargs = {"password": {"write_only": True}}
-        
-    def create(self, validated_data):
-        # Use get_user_model().objects.create_user to create a user
-        user = CustomUser.objects.create_user(
-            username=validated_data[username], 
-            email=validated_data[email], 
-            password=validated_data[password])
-        # Token.objects.create(user=user)
-        return user
+        fields = ['username', 'password', 'email']
+        extra_kwargs = {'password': {'write_only': True}}
     
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(**validated_data)
+        Token.objects.create(user=user)
+        return user
+
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField(write_only=True)
+    
+    def validate(self, data):
+        user = authenticate(username=data['username'], password=data['password'])
+        if not user:
+            raise serializers.ValidationError("Invalid credentials")
+        token, _ = Token.objects.get_or_create(user=user)
+        return {'user': user, 'token': token.key}
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,31 +1,25 @@
-from django.shortcuts import render
-from rest_framework.authtoken.models import Token
+from rest_framework.generics import CreateAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from .models import CustomUser
 from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
 
-class RegisterView(APIView):
-    def post(self, request):
-        serializer = RegisterSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-    
+class RegisterView(CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
+
 class LoginView(APIView):
+    permission_classes = [AllowAny]
     def post(self, request):
         serializer = LoginSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        user = CustomUser.objects.filter(username=serializer.validated_data["username"]).first()
-        if user in user.check_password(serializer.validated_data["password"]):
-            token, created = Token.objects.get_or_create(user=user)
-            return Response({"token": token.key}, status=200)
-        return Response({"error": "Invalid data"}, status=400)
-    
+        return Response(serializer.validated_data)
+
 class ProfileView(APIView):
     permission_classes = [IsAuthenticated]
-
+    
     def get(self, request):
         serializer = UserSerializer(request.user)
         return Response(serializer.data)
